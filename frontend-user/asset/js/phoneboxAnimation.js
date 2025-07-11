719800a386b71be9fbea70a735eb3cdbf12f7f90
Thinh Phuc <duonge12@gmail.com>
2025-07-11 23:56:55 +0700

    fix slide down position

--- frontend-user/asset/js/phoneboxAnimation.js
+++ frontend-user/asset/js/phoneboxAnimation.js
@@ -17,10 +17,12 @@ const animate = () => {
 	if (isAtFullScreen && scrolled <= 100) {
 		slideDown.style.visibility = "visible";
 		slideUp.style.visibility = "visible";
-		console.log(scrolled);
-		slideDown.style.transform = `translateY(${scrolled * 3}%)`;
-		slideUp.style.transform = `translateY(${scrolled * 1}%)`;
+		const slideDownDistance = Math.ceil(scrolled * 3) * 1.1;
+		const slideUpDistance = Math.ceil(scrolled * 1) * 1.1;
+		slideDown.style.transform = `translateY(${slideDownDistance}%)`;
+		slideUp.style.transform = `translateY(${slideUpDistance}%)`;
 		title.style.transform = `translateY(${scrolled}%)`;
+		console.log("a", Math.ceil(scrolled * 3));
 		title.style.opacity = scrolled / 100;
 	}
 
